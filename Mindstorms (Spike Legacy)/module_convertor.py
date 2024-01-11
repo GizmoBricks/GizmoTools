9602201380d0f2c3b14e1c6fba4591ab6fe22ae1
GizmoBricks <127412675+GizmoBricks@users.noreply.github.com>
2024-01-11 11:58:12 +0100

    Delete Mindstorms (Spike Legacy)/module_convertor.py

--- Mindstorms (Spike Legacy)/module_convertor.py
+++
@@ -1,7 +0,0 @@
-# Replace `64646` with ID of your module.
-with open('/projects/64646/__init__.py', 'r') as file:
-    module_name = file.readline().rstrip().split()[1]
-    with open('/{}.py'.format(module_name), 'w') as write_file:
-        for line in file:
-            write_file.write(line)
-print('Done')
